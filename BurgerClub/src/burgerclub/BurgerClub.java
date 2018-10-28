d5f1b4bf0e304c8517907cc736f94f0af866981a
dubkovigorek <44555277+dubkovigorek@users.noreply.github.com>
2018-10-28 23:24:27 +0300

    Add files via upload

---
+++ BurgerClub/src/burgerclub/BurgerClub.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package burgerclub;
+
+
+/**
+ *
+ * @author Igor Dubkov
+ */
+import my.numberaddition.BurgerClubUI;
+public class BurgerClub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BurgerClubUI frame = new BurgerClubUI();
+        frame.setVisible(true);
+       
+        
+    }
+    
+}
